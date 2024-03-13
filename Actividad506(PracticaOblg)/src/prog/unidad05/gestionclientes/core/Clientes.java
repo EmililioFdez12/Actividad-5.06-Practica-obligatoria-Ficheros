491dc7c1b70a38f0106c0ebeb6d6d7c069eecb88
EmilioFdez12 <emiliofdezglld12@gmail.com>
2024-03-13 21:59:10 +0100

    ProveedorFichero pruebas

--- Actividad506(PracticaOblg)/src/prog/unidad05/gestionclientes/core/Clientes.java
+++ Actividad506(PracticaOblg)/src/prog/unidad05/gestionclientes/core/Clientes.java
@@ -5,124 +5,119 @@ import java.util.List;
 
 public class Clientes {
 
-  private ProveedorAlmacenamientoClientes proveedorAlmacenamiento;
-  private List<Cliente> listaClientes;
+	private ProveedorAlmacenamientoClientes proveedorAlmacenamiento;
+	private List<Cliente> listaClientes;
+	
+	public Clientes(ProveedorAlmacenamientoClientes proveedorAlmacenamiento) {
+		if (proveedorAlmacenamiento != null) {
+			this.proveedorAlmacenamiento = proveedorAlmacenamiento;
+			this.listaClientes = new ArrayList<>();
+		} else {
+			throw new NullPointerException();
+		}
+		
+		try {
+			proveedorAlmacenamiento.getAll();
+		} catch (ProveedorAlmacenamientoClientesException e) {
+			
+		}
+	}
 
-  public Clientes(ProveedorAlmacenamientoClientes proveedorAlmacenamiento){
-    if (proveedorAlmacenamiento != null) {
-        this.proveedorAlmacenamiento = proveedorAlmacenamiento;
-        this.listaClientes = new ArrayList<>();
-    } else {
-        throw new NullPointerException();
-    }
-}
+	public void addCliente(Cliente cliente)
+			throws ClientesException, NullPointerException, ProveedorAlmacenamientoClientesException {
+		if (cliente == null) {
+			throw new NullPointerException();
+		}
 
-  public void addCliente(Cliente cliente)
-      throws ClientesException, NullPointerException, ProveedorAlmacenamientoClientesException {
-    if (cliente == null) {
-      throw new NullPointerException();
-    }
+		if (listaClientes.contains(cliente)) {
+			throw new ClientesException();
+		}
 
-    if (listaClientes.contains(cliente)) {
-      throw new ClientesException();
-    }
+		listaClientes.add(cliente);
 
-    listaClientes.add(cliente);
+		Cliente[] arrayClientes = listaClientes.toArray(new Cliente[0]);
+		proveedorAlmacenamiento.saveAll(arrayClientes);
+	}
 
-    Cliente[] arrayClientes = listaClientes.toArray(new Cliente[0]);
-    proveedorAlmacenamiento.saveAll(arrayClientes);
-  }
+	public void updateCliente(Cliente cliente)
+			throws ClientesException, NullPointerException, ProveedorAlmacenamientoClientesException {
+		if (cliente == null) {
+			throw new NullPointerException();
+		}
 
-  public void updateCliente(Cliente cliente)
-      throws ClientesException, NullPointerException, ProveedorAlmacenamientoClientesException {
-    if (cliente == null) {
-      throw new NullPointerException();
-    }
+		boolean nifExiste = false;
+		for (Cliente cliente2 : listaClientes) {
+			if (cliente2.getNif().equals(cliente.getNif())) {
+				nifExiste = true;
+			}
+		}
 
-    boolean nifExiste = false;
-    for (Cliente cliente2: listaClientes) {
-      if (cliente2.getNif().equals(cliente.getNif())) {
-        nifExiste = true;
-      }
-    }
-    
-    if (!nifExiste) {
-      throw new ClientesException();
-    }
-        
-      for (int i = 0; i < listaClientes.size(); i++) {
-        // Si el Dni del cliente de la lista coincide con el Dni del cliente que hemos
-        // pasado
-        if (listaClientes.get(i).getNif().equals(cliente.getNif())) {
-          listaClientes.set(i, cliente);
-        }
-      }
+		if (!nifExiste) {
+			throw new ClientesException();
+		}
 
-    // Guardar todos los clientes, incluido el nuevo
-    Cliente[] arrayClientes = listaClientes.toArray(new Cliente[0]);
-    proveedorAlmacenamiento.saveAll(arrayClientes);
-  }
+		for (int i = 0; i < listaClientes.size(); i++) {
+			// Si el Dni del cliente de la lista coincide con el Dni del cliente que hemos
+			// pasado
+			if (listaClientes.get(i).getNif().equals(cliente.getNif())) {
+				listaClientes.set(i, cliente);
+			}
+		}
 
-  /**
-   * 
-   * @param nif
-   * @throws ClientesException
-   * @throws ProveedorAlmacenamientoClientesException
-   */
-  public void removeCliente(String nif) throws ClientesException, ProveedorAlmacenamientoClientesException {
-    if (nif == null) {
-        throw new NullPointerException();
-    }
+		// Guardar todos los clientes, incluido el nuevo
+		Cliente[] arrayClientes = listaClientes.toArray(new Cliente[0]);
+		proveedorAlmacenamiento.saveAll(arrayClientes);
+	}
 
-    boolean clienteEncontrado = false;
-    // Hacemos copia de la lista de clientes, la recorremos
-    // Y si coinciden los nif, y en la posicion de la lista de la copia borramos el cliente
-    // en la lista de clientes
-    List<Cliente> copiaClientes = new ArrayList<>(listaClientes);
-    for (Cliente cliente : copiaClientes) {
-        if (cliente.getNif().equals(nif)) {
-            clienteEncontrado = true;
-            listaClientes.remove(cliente);
-        }
-    }
+	/**
+	 * 
+	 * @param nif
+	 * @throws ClientesException
+	 * @throws ProveedorAlmacenamientoClientesException
+	 */
+	public void removeCliente(String nif) throws ClientesException, ProveedorAlmacenamientoClientesException {
+		if (nif == null) {
+			throw new NullPointerException();
+		}
 
-    if (!clienteEncontrado) {
-        throw new ClientesException();
-    }
+		boolean clienteEncontrado = false;
+		// Hacemos copia de la lista de clientes, la recorremos
+		// Y si coinciden los nif, y en la posicion de la lista de la copia borramos el
+		// cliente
+		// en la lista de clientes
+		List<Cliente> copiaClientes = new ArrayList<>(listaClientes);
+		for (Cliente cliente : copiaClientes) {
+			if (cliente.getNif().equals(nif)) {
+				clienteEncontrado = true;
+				listaClientes.remove(cliente);
+			}
+		}
 
-    // Guardar todos los clientes, incluido el nuevo
-    Cliente[] arrayClientes = listaClientes.toArray(new Cliente[0]);
-    proveedorAlmacenamiento.saveAll(arrayClientes);
-}
+		if (!clienteEncontrado) {
+			throw new ClientesException();
+		}
 
-  public Cliente getByNif(String nif) throws ProveedorAlmacenamientoClientesException {
-    Cliente cliente = null;
-    // Obtener todos los clientes del almacenamiento
-    Cliente[] clientesArray = proveedorAlmacenamiento.getAll();
-    for (Cliente clientes : clientesArray) {
-      if (clientes.getNif().equals(nif)) {
-        cliente = clientes;
-      }
-    }
-    return cliente;
+		// Guardar todos los clientes, incluido el nuevo
+		Cliente[] arrayClientes = listaClientes.toArray(new Cliente[0]);
+		proveedorAlmacenamiento.saveAll(arrayClientes);
+	}
 
-  }
+	public Cliente getByNif(String nif) throws ProveedorAlmacenamientoClientesException {
+		Cliente cliente = null;
+		// Obtener todos los clientes del almacenamiento
+		Cliente[] clientesArray = proveedorAlmacenamiento.getAll();
+		for (Cliente clientes : clientesArray) {
+			if (clientes.getNif().equals(nif)) {
+				cliente = clientes;
+			}
+		}
+		return cliente;
 
-  public void visita(VisitadorClientes visitador){
-    for (Cliente cliente : listaClientes) {
-      visitador.visita(cliente);
-    }
-  }
+	}
 
-  public static void main(String[] args)
-      throws NullPointerException, ClientesException, ProveedorAlmacenamientoClientesException {
-    ProveedorAlmacenamientoClientesMock proveedor = new ProveedorAlmacenamientoClientesMock();
-    Clientes clientes = new Clientes(proveedor);
-    String nif = "62527456R";
-    Cliente cliente = new Cliente(nif, "Makako", "Pako", 10, 12345.67, true);
-    
-    clientes.addCliente(cliente);
-    clientes.removeCliente(nif);
-
-  }
+	public void visita(VisitadorClientes visitador) {
+		for (Cliente cliente : listaClientes) {
+			visitador.visita(cliente);
+		}
+	}
 }
--- Actividad506(PracticaOblg)/src/prog/unidad05/gestionclientes/impl/ProveedorAlmacenamientoClientesFichero.java
+++ Actividad506(PracticaOblg)/src/prog/unidad05/gestionclientes/impl/ProveedorAlmacenamientoClientesFichero.java
@@ -1,12 +1,21 @@
 package prog.unidad05.gestionclientes.impl;
 
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import prog.unidad05.gestionclientes.core.Cliente;
 import prog.unidad05.gestionclientes.core.ProveedorAlmacenamientoClientes;
 import prog.unidad05.gestionclientes.core.ProveedorAlmacenamientoClientesException;
+import prog.unidad05.gestionclientes.core.Clientes;
+import prog.unidad05.gestionclientes.core.ClientesException;
 
 /**
  * Proveedor de almacenamiento de clientes que almacena los datos de los
@@ -15,35 +24,73 @@ import prog.unidad05.gestionclientes.core.ProveedorAlmacenamientoClientesExcepti
  */
 public class ProveedorAlmacenamientoClientesFichero implements ProveedorAlmacenamientoClientes {
 
-  private Set<Cliente> clientes;
+	private String rutaFichero = "clientes.dat";
+	private Set<Cliente> clientes;
 
-  /**
-   * Constructor con ruta
-   * 
-   * @param rutaFichero Ruta al fichero a emplear para leer y almacenar los
-   *                    clientes
-   * @throws NullPointerException Si la ruta no es nula
-   */
-  public ProveedorAlmacenamientoClientesFichero(String rutaFichero) {
-    this.clientes = new HashSet<>();
-  }
-  
-  public boolean contains(Cliente cliente){
-  	return clientes.contains(cliente);
-  }
-  
-  public int size() {
-  	return clientes.size();
-  }
+	/**
+	 * Constructor con ruta
+	 * 
+	 * @param rutaFichero Ruta al fichero a emplear para leer y almacenar los
+	 *                    clientes
+	 * @throws NullPointerException Si la ruta no es nula
+	 */
+	public ProveedorAlmacenamientoClientesFichero(String rutaFichero) {
+		this.rutaFichero = rutaFichero;
+		this.clientes = new HashSet<>();
+	}
 
-  @Override
-  public Cliente[] getAll() throws ProveedorAlmacenamientoClientesException {
-    return clientes.toArray(new Cliente[0]);
-  }
+	public boolean contains(Cliente cliente) {
+		return clientes.contains(cliente);
+	}
 
-  @Override
-  public void saveAll(Cliente[] clientes) throws NullPointerException, ProveedorAlmacenamientoClientesException {
-  	this.clientes = Set.of(clientes);
-  }
- 
+	public int size() {
+		return clientes.size();
+	}
+
+	@Override
+	public Cliente[] getAll() throws ProveedorAlmacenamientoClientesException {
+		List<String> listaLineas = new ArrayList<>();
+		try (BufferedReader flujoEntrada = new BufferedReader(new FileReader(rutaFichero))) {
+			String linea = null;
+			do {
+				linea = flujoEntrada.readLine();
+				listaLineas.add(linea);
+			} while (linea != null);
+
+		} catch (FileNotFoundException e) {
+			System.out.println("No se ha encontrado el fichero");
+		} catch (IOException e) {
+			System.out.println("No se puede leer el archivo");
+		}
+		return clientes.toArray(new Cliente[0]);
+	}
+
+	@Override
+	public void saveAll(Cliente[] clientes) throws NullPointerException, ProveedorAlmacenamientoClientesException {
+		try (PrintWriter flujoSalida = new PrintWriter(new FileWriter(rutaFichero))) {    
+          for (Cliente cliente: clientes) {
+						flujoSalida.println(cliente.toString());
+					}           
+    } catch (IOException e) {
+      System.out.println("No se ha podido crear el archivo.");
+      e.printStackTrace();
+    }
+		this.clientes = Set.of(clientes);
+	}
+
+	public static void main(String[] args)
+			throws ProveedorAlmacenamientoClientesException, NullPointerException, ClientesException {
+		Cliente cliente = new Cliente("77687485V", "Pepin", "Rodriguez", 20, 25.5, false);
+		Cliente cliente2 = new Cliente("67071237V", "Lolito", "Fernandez", 500, 100000, true);
+		String ruta = "clientes.dat";
+
+		ProveedorAlmacenamientoClientesFichero proveedor = new ProveedorAlmacenamientoClientesFichero(ruta);
+		Clientes clientes = new Clientes(proveedor);
+		clientes.addCliente(cliente);
+		clientes.addCliente(cliente2);
+		
+		Cliente[] arrayCliente = proveedor.getAll();
+		proveedor.saveAll(arrayCliente);
+	}
+
 }
